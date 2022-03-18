070414bc65322a5c8c49a289fe2a666378d31691
Ernest Torz <ernesttorz@gmail.com>
2022-03-18 16:15:56 +0100

    nowe

--- UserControls/Item.xaml.cs
+++ UserControls/Item.xaml.cs
@@ -62,7 +62,7 @@ namespace Kalendarz_T_K.UserControls
         }
         public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(Item));
 
-        private void MenuButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void MenuButton_MouseDoubleClick_Trash(object sender, MouseButtonEventArgs e)
         {
             StackPanel parent = this.Parent as StackPanel;
             parent.Children.Remove(this);
